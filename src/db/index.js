56cbcd119bb22a2310d5d5cd39ea570b59f39a6b
Vishalra-i <vishalrai0392@gmail.com>
2024-03-08 17:58:23 +0530

    Utils update

---
+++ src/db/index.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async ()=>{
+    try {
+        const conectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`\n MONGODB Connected !! DB Host:${conectionInstance.connection.host}`)
+    } catch (error) {
+        console.log("MONGODB Connnection Failed",error)
+        process.exit(1)
+    }
+}
+
+export default connectDB
---
+++ src/utils/ApiError.js
@@ -0,0 +1,23 @@
+class ApiError extends Error {
+    constructor(
+        stausCode,
+        message = 'Something went wrong',
+        error = [],
+        stack = '' 
+        
+    ){
+        super(message)
+        this.statusCode = stausCode
+        this.data = null 
+        this.message = message
+        this.success = false
+        this.error = error
+
+        if(stack){
+            this.stack = stack
+        }else{
+            Error.captureStackTrace(this, this.constructor)
+        
+        }
+    }       
+}
